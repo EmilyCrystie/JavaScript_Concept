adf9f55286e7c3995ff5e3f3eedf91831af9f167
Emily Crystie <emily_crystie@hotmail.com>
2021-09-17 06:55:34 -0300

    while

---
+++ 4. While/index.js
@@ -0,0 +1,6 @@
+let spaceshipName = prompt("Olá, iniciamos a decolagem, ajustando rota de vôo, por favor,\nDigite o nome da sua espaço-nave:")
+let espaceFold = prompt("Deseja entrar em dobra espacial? \n1-Sim \n2-Não")
+
+while(espaceFold = 1){
+    alert("Contabilizando dobra")
+}

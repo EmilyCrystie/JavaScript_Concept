f7bbf9655cd347520cd34aa6723b511568a84667
Emily Crystie <emily_crystie@hotmail.com>
2021-09-20 06:42:13 -0300

    exercício

--- 4. While/index.js
+++ 4. While/index.js
@@ -1,6 +1,14 @@
-let spaceshipName = prompt("Olá, iniciamos a decolagem, ajustando rota de vôo, por favor,\nDigite o nome da sua espaço-nave:")
-let espaceFold = prompt("Deseja entrar em dobra espacial? \n1-Sim \n2-Não")
 
-while(espaceFold = 1){
-    alert("Contabilizando dobra")
+let warpCount = 0
+let spaceFold = ""
+
+let spaceship = prompt("Olá, iniciamos a decolagem, ajustando rota de vôo, por favor,\nDigite o nome da sua espaço-nave: ")
+
+spaceFold = prompt("Deseja entrar em dobra espacial?\n1-Sim \n2-Não")
+
+while(spaceFold == "1"){
+    warpCount += 1
+    spaceFold = prompt("Deseja realizar a próxima dobra?\n1-Sim \n2-Não")
 }
+
+alert("Nave: " + spaceship + "\nQuantidade de dobras: " + warpCount)

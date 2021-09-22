43a66d7b06ce16d55bab33fb3c6f56aebf12864e
Emily Crystie <emily_crystie@hotmail.com>
2021-09-22 07:01:40 -0300

    for loop

---
+++ 5. For/index.js
@@ -0,0 +1,14 @@
+let spaceship = prompt("Olá, digite o nome da sua nave: ")
+let character = prompt("Qual caracter deseja substituir?") 
+let newCharacter = prompt("Por qual caracter deseja substituir o anterior?")
+let newSpaceship = ""
+
+for (let pos = 0; pos < spaceship.length; pos++) {  //pos = abreviação de position
+    if(spaceship[pos] == character){
+        newSpaceship += newCharacter[pos]
+    } else {
+        newSpaceship += spaceship[pos]
+    } 
+}
+
+alert("O novo nome da nave é " + newSpaceship)

5327cd1701bc8efb3763d9a9be04e512e6730302
Emily Crystie <emily_crystie@hotmail.com>
2021-10-15 05:58:33 -0300

    versaoEmily

--- 7. Funções/index.js
+++ 7. Funções/index.js
@@ -4,18 +4,42 @@ let acceleration = 5
 let newVelocity = ""
 
 let menu = prompt("Menu de opções \nDigite o número da opção desejada: \n" + 1 + ". Acelerar a nave em 5km/s \n" + 2 + ". Desacelerar em 5km/s\n" + 3 + ". Imprimir dados de bordo\n" + 4 + ". Sair do programa")
-confirm("Deu certo?")
 
-function speedUp (newVelocity) {
+function speedUp(newVelocity) {
     newVelocity = initialVelocity + acceleration
 }
 
 function slowDown(newVelocity) {
-   if (initialVelocity=0) {
-       
+   if (newVelocity>0) {
+        newVelocity - acceleration
    }
-    newVelocity - acceleration
 }
 
+function dataship(spaceship, newVelocity) {
+    prompt("Nave: " + spaceship + "\nVelocidade atual: " + newVelocity)
+}
 
+function logout(velocity) {
+    newVelocity +- acceleration;
+    if (newVelocity==5) {
+        newVelocity = initialVelocity
+    }
+}
 
+switch (menu) {
+    case 1:
+        speedUp
+        break;
+    case 2:
+        slowDown
+        break;
+    case 3:
+        dataship
+        break;
+    case 4:
+        logout
+        break;
+    default:
+        alert("Opção não encontrada")
+        break;
+}

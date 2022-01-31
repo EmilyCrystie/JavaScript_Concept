cb07dcde5246ed4be8258ae36c778a917d3b23ae
Emily Crystie <emily_crystie@hotmail.com>
2022-01-31 10:53:32 -0300

    exercicio_hof

--- 8. High Order Functions/index.js
+++ 8. High Order Functions/index.js
@@ -1,9 +1,12 @@
-let maxVelocity = 150
-
-function desaceleration(newVelocity, data){
-    
+function slowDown(velocity, printer) {
+    let deceleration = 20
+    while(velocity > 0) {
+        printer(velocity)
+        velocity -= deceleration
+    }
+    alert("Nave parada. As comportas podem ser abertas.")
 }
-
-function newVelocity {
-
-}
+let spaceshipVelocity = 150
+slowDown(spaceshipVelocity, function(velocity) {
+    console.log("Velocidade atual: " + velocity)
+})

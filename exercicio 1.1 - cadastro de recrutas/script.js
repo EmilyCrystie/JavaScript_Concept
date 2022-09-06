b7bfdcf97bce3c279becddf1527fec65985891f4
Emily Crystie <emily_crystie@hotmail.com>
2022-09-06 16:01:58 -0300

    :books: atualização readme
    inserção da questão

--- exercicio 1.1 - cadastro de recrutas/script.js
+++ exercicio 1.1 - cadastro de recrutas/script.js
@@ -1,3 +1,13 @@
+/*Escreva um programa em javascript que permita salvar informações de um recruta. As informações a serem salvas são:
+
+- o primeiro nome
+- o sobrenome
+- o campo de estudo
+- o ano de nascimento
+
+Depois o programa deve exibir o nome completo do recruta, seu campo de estudo e sua idade (apenas baseada no ano de nascimento).*/
+
+
 let nome = prompt("Informe seu nome: ");
 let sobrenome = prompt("Agora seu sobrenome: ");
 let campo_estudo = prompt("Informe seu campo de estudo: ");

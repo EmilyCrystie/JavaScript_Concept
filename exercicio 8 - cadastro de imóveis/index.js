a1489b74e592299487bef39551837026910b72fc
Emily Crystie <emily_crystie@hotmail.com>
2022-09-16 15:55:04 -0300

    continuação das aulas

---
+++ exercicio 8 - cadastro de imóveis/index.js
@@ -0,0 +1,30 @@
+/*
+Escreva um programa em javascript que funcione como um cadastro de imóveis e atenda aos seguintes requisitos:
+
+- Deve ter um menu interativo que sempre é exibido até que o usuário escolha sair.
+- O menu interativo deve mostrar no topo a quantidade de imóveis cadastrados.
+- O menu deve ter a opção de salvar um imóvel.
+- Para salvar um novo imóvel o programa deve pedir as seguintes informações:
+    - Nome do proprietário.
+    - Quantidade de quartos.
+    - Quantidade de banheiros.
+    - Se possui garagem.
+- O menu também deve ter a opção de mostrar todos os imóveis salvos.
+*/
+
+//menu
+    //qtd de imóveis cadastrados
+    //opções
+        //novo imóvel
+            //nome prop.
+            //qtd quartos
+            //qtd banheiros
+            //garagem: sim/não
+        //mostruário
+        //sair
+
+
+do{
+    
+
+} while(opcao != 3)
